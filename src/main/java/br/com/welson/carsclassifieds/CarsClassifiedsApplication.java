a68e40459480a63c09a0911591b717365d3430e5
Welson Teles <welsonlimawlsn@gmail.com>
2018-04-17 09:17:41 -0300

    starts the project

---
+++ src/main/java/br/com/welson/carsclassifieds/CarsClassifiedsApplication.java
@@ -0,0 +1,12 @@
+package br.com.welson.carsclassifieds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsClassifiedsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsClassifiedsApplication.class, args);
+    }
+}
